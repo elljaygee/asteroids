a148624634c24c7c1bc3ca347cf040d494ad4d15
elljaygee <lilithknits@gmail.com>
2025-02-24 13:27:54 +0000

    added Player class

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init() # initialise pygame module
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # set initial display size
 
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
 
@@ -21,6 +23,7 @@ def main():
                 if event.type == pygame.QUIT:
                     return
         screen.fill((0, 0, 0)) # set the screen colour to black
+        player.draw(screen) # draw the player object in the centre of the screen
         pygame.display.flip() # refresh the screen
         dt = clock.tick(60) / 1000 #
 
---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Player(CircleShape):
+
+    # initialises the Player class and pulls variables from the parent CircleShape
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.timer = 0
+
+    # creates a triangle shape for the player
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    # overrides the draw function from the parent class CircleShape using the points given in triangle
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
