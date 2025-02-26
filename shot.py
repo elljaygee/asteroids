9ab904d31a071f8e02e54428781ff81a29ea25be
lilith green <127050158+elljaygee@users.noreply.github.com>
2025-02-26 14:34:29 +0000

    added Shot class

---
+++ shot.py
@@ -0,0 +1,17 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Shot(CircleShape):
+
+    # initialises the Player class and pulls variables from the parent CircleShape
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+    
