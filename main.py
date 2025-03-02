35ff87d535f780bb2f6154ff2a08a37542cca484
elljaygee <lilithknits@gmail.com>
2025-03-02 09:49:59 +0000

    added split function

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
             
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                  
 
